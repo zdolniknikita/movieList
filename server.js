0f6de0756494ca4d7fbfad10fb7e05a14bb8970f
Nykyta Zdolnyk <zdolniknikita@gmail.com>
2020-02-13 16:30:07 +0200

    update

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static(__dirname + '/dist/movielist'))
+app.use(express.static(__dirname + '/dist/movieList'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/movielist/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/movieList/index.html'))
 })
 
 app.listen(process.env.PORT || 8080)
